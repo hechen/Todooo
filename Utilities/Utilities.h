2aed4face4fb5bdae31c8fb7ee9c8b673241ab8e
chen <hechen.dream@gmail.com>
2022-03-16 19:56:02 -0700

    remove third party files

--- Utilities/Utilities.h
+++ Utilities/Utilities.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char UtilitiesVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Utilities/PublicHeader.h>
 
-#import "ITSwitch.h"
